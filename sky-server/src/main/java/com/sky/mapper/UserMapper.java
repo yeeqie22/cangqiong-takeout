6acc632a3f6ad79ad67e6e4f9604aec558896f73
qieyi <yi.qie@outlook.com>
2023-09-20 17:54:57 +0800

    微信用户登陆业务代码开发

---
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -0,0 +1,22 @@
+package com.sky.mapper;
+
+import com.sky.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+    /**
+     * 根据openId查询用户
+     * @param openId
+     * @return
+     */
+    @Select("select * from user where openid = #{openId}")
+    User getByOpenId(String openId);
+
+    /**
+     * 插入数据
+     * @param user
+     */
+    void insert(User user);
+}
