cab9b2cce3d0e6dad4f933889bc182388b86fdee
qieyi <yi.qie@outlook.com>
2023-09-29 10:46:26 +0800

    查询历史订单、查询订单详情、取消订单业务代码开发

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -1,7 +1,10 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface OrderMapper {
@@ -10,4 +13,22 @@ public interface OrderMapper {
      * @param orders
      */
     void insert(Orders orders);
+    /**
+     * 分页条件查询并按下单时间排序
+     * @param ordersPageQueryDTO
+     */
+    Page<Orders> pageQuery(OrdersPageQueryDTO ordersPageQueryDTO);
+    /**
+     * 根据id查询订单
+     * @param id
+     */
+    @Select("select * from orders where id=#{id}")
+    Orders getById(Long id);
+
+    /**
+     * 更新订单信息
+     * @param orders
+     */
+    //TODO 补全sql语句
+    void update(Orders orders);
 }
