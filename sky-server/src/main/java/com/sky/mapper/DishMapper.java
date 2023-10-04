14e57f2adab683dd9950d00ce1892b9434e8db33
qieyi <yi.qie@outlook.com>
2023-10-04 12:53:29 +0800

    工作台业务代码开发

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -13,6 +13,7 @@ import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface DishMapper {
@@ -81,4 +82,10 @@ public interface DishMapper {
      */
     @Update("update dish set status = #{status} where id = #{id}")
     void setStatus(Integer status, Long id);
+    /**
+     * 根据条件统计菜品数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
